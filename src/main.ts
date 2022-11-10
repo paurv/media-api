70c3700d9bfa94e2ee43aaa4bde5dc033c67f43f
Paurv <ana.posadasr97@gmail.com>
2022-11-10 03:22:14 -0600

    feat: swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Demo Application')
+    .setDescription('Demo API Application')
+    .setVersion('v1')
+    .addTag('media')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
